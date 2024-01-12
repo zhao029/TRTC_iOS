295292b091d76d7e3730da36c9c18f5fbeda1d87
chengyuzhao <chengyuzhao@tencent.com>
2024-01-12 17:36:34 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "TRTCKit",
+    name: "TRTCProfessionalKit",
     platforms: [
         .iOS(.v9)
     ],
     products: [
         .library(
-            name: "TRTC",
-            targets: ["TXLiteAVSDK_TRTC_SPM", "TXSoundTouch", "TXFFmpeg"]),
+            name: "TRTCProfessional",
+            targets: ["TXLiteAVSDK_Professional_SPM"]),
         .library(
             name: "TRTCReplayKitExt",
             targets: ["TXLiteAVSDK_ReplayKitExt"]),
@@ -18,29 +18,19 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "TXLiteAVSDK_TRTC",
-            url: "https://sdk-liteav-1252463788.cos.ap-hongkong.myqcloud.com/customer/HelperTest/SPM/TXLiteAVSDK_TRTC.xcframework.zip",
-            checksum: "48269bdbfa4b1c374f5016ed142962c848b7036578ad01f09331ff9a94208725"
+            name: "TXLiteAVSDK_Professional",
+            url: "https://liteav.sdk.qcloud.com/customer/Pococha/SPM/TXLiteAVSDK_Professional.xcframework.zip",
+            checksum: "05f35def69bf05f5f2491f48628015f0a45dd0b5d14289ff18cde566baf24f71"
            ),
         .binaryTarget(
-            name: "TXSoundTouch",
-            url: "https://sdk-liteav-1252463788.cos.ap-hongkong.myqcloud.com/customer/HelperTest/SPM/TXSoundTouch.xcframework.zip",
-            checksum: "4870aebc2758afd0f0756db0f4baa478338c45dde6fa45cf2fd7a9b5c5b76ff5"
-            ),
-        .binaryTarget(
-            name: "TXFFmpeg",
-            url: "https://sdk-liteav-1252463788.cos.ap-hongkong.myqcloud.com/customer/HelperTest/SPM/TXFFmpeg.xcframework.zip",
-            checksum: "a9cebf1f50587f9dca69d5ba61c9a2c20a6a80090369056365d2bead660a6e4e"
-            ),
-        .binaryTarget(
             name: "TXLiteAVSDK_ReplayKitExt",
-            url: "https://sdk-liteav-1252463788.cos.ap-hongkong.myqcloud.com/customer/HelperTest/SPM/TXLiteAVSDK_ReplayKitExt.xcframework.zip",
-            checksum: "1fa5402691bc4150c73d4535ec040ca54f2859af74af9ef8a80605a9b302284d"
+            url: "https://liteav.sdk.qcloud.com/customer/Pococha/SPM/TXLiteAVSDK_ReplayKitExt.xcframework.zip",
+            checksum: "47be461e7520212a28417f16d277a0449cf2cafd743e116db00b64a89f7cc672"
             ),
         .target(
-            name: "TXLiteAVSDK_TRTC_SPM",
+            name: "TXLiteAVSDK_Professional_SPM",
             dependencies: [
-                .target(name: "TXLiteAVSDK_TRTC")
+                .target(name: "TXLiteAVSDK_Professional")
             ],
             sources: ["trtc_swift_package.swift"],
             linkerSettings: [
